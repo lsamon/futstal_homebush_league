ec854be5249512c6d25bf261b8ff78e6c370ec8d
lsamon <louis.amon@amazutech.net>
2016-10-13 16:50:04 +1100

    CRUD Operations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  attr_accessor :err_message
   before_action :check_for_admin, :only => ['new', 'edit', 'create', 'update']
 
   def index
@@ -22,25 +23,8 @@ class GamesController < ApplicationController
     team_a_id = params[:game][:team_a_id]
     team_b_id = params[:game][:team_b_id]
 
-    if team_a_id.present? && team_b_id.present?
-      if team_a_id != team_b_id
-        team_a_div = (Team.find team_a_id).division.division_number
-        team_b_div = (Team.find team_b_id).division.division_number
-
-        if team_a_div == team_b_div
-          @game.save
-          # Game.add_points(@game)
-          redirect_to @game
-        else
-          redirect_to_new("Error! Teams in different divisions cannot be matched!! Check Divisions menu to match teams")
-        end
-      else
-        redirect_to_new("Error! Team cannot play itself!!")
-      end
-    else
-      redirect_to_new("Error! Both Teams need to be set to have a match!!")
-    end
-
+    validate_game(@game, team_a_id, team_b_id)
+    redirect_to_new(@err_message) unless @err_message.nil?
   end
 
   def show
@@ -48,24 +32,67 @@ class GamesController < ApplicationController
   end
 
   def edit
+    @game = Game.find params[:id]
+  end
+
+  def update
+    game = Game.update_attributes game_params
+    team_a = game.team_a_id
+    team_b = game.team_b_id
+
+    validate_game(game, team_a_id, team_b_id)
+    redirect_to_new(@err_message) unless @err_message.nil?
   end
 
   def destroy
+    game = Game.find params[:id]
+    game.destroy
+
+    redirect_to games_fixtures_path
   end
+
   private
 
   def game_params
     params.require(:game).permit(:team_a_id, :team_b_id, :team_a_score, :team_b_score, :game_date)
   end
 
-  def check_divisions
-  end
-
-  def check_same_team
-  end
+  # def check_divisions(teama,teamb, msg)
+  # if teama.division_number == teamb.division_number
+  #   redirect_to_new msg
+  # end
+  # end
+  #
+  # def check_same_team(teama, teamb, msg)
+  #   if teama.id == teamb.id
+  #     redirect_to_new msg
+  #
+  # end
 
   def redirect_to_new(message)
     flash[:error] = message
     render 'new'
   end
+
+  def validate_game(game, team_a_id, team_b_id)
+    if team_a_id.present? && team_b_id.present?
+      if team_a_id != team_b_id
+        team_a_div = (Team.find team_a_id).division.division_number
+        team_b_div = (Team.find team_b_id).division.division_number
+
+        if team_a_div == team_b_div
+          game.save
+          # Game.add_points(@game)
+          redirect_to game
+        else
+          @err_message = "Error! Teams in different divisions cannot be matched!! Check Divisions menu to match teams"
+        end
+      else
+        @err_message = "Error! Team cannot play itself!!"
+      end
+    else
+      @err_message = "Error! Both Teams need to be set to have a match!!"
+    end
+  end
+
 end
